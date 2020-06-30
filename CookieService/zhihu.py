9224dab71bf8ef0731a5f737a2e29639aa8e40d6
xuelin <19946174874@163.com>
2020-07-01 01:20:35 +0800

    回退chrome78版本，新增两种方式处理知乎登陆识别webdriver反爬机制

--- CookieService/zhihu.py
+++ CookieService/zhihu.py
@@ -1,17 +1,34 @@
 
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from base_service import BaseService
 from selenium.webdriver.common.keys import Keys
 
 class ZhihuLoginService(BaseService):
+    '''
+    通过chrome.exe --remote-debugging-port=9222 结合 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")反爬
+    or
+    通过chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    '''
     name = "zhihu"
 
     def __init__(self):
         chrome_options = Options()
+        # chrome_options.headless = True
         chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        self.brower = webdriver.Chrome(executable_path="webdriver/chromedriver.exe",chrome_options=chrome_options)
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # chrome_options.add_experimental_option('useAutomationExtension', False)
+        # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        self.brower = webdriver.Chrome(executable_path="webdriver/chromedriver.exe",chrome_options=chrome_options,options=option)
+        # self.brower.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        # "source": """
+        #     Object.defineProperty(navigator, 'webdriver', {
+        #         get: () => undefined
+        #     })
+        # """
+        # })
 
     def check_login(self):
         try:
@@ -32,7 +49,8 @@ class ZhihuLoginService(BaseService):
             # self.brower.find_element_by_xpath("//form//botton[@type='submit']").click()
             botton = self.brower.find_element_by_css_selector(".Button.SignFlow-submitButton.Button--primary.Button--blue")
             botton.click()
-            print("hxl")
+            time.sleep(5)
+            print(self.brower.page_source)
 
 if __name__ == "__main__":
     a = ZhihuLoginService()
