7c3726ea157937c4b7c31397c4576d105f9d0e97
xuelin <19946174874@163.com>
2020-07-02 23:50:54 +0800

    新增知乎使用zheye倒立文字识别和超级鹰验证码识别模拟登录

--- CookieService/zhihu.py
+++ CookieService/zhihu.py
@@ -1,10 +1,14 @@
 
 import time
+import base64
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from base_service import BaseService
 from selenium.webdriver.common.keys import Keys
+from zheye import zheye
+from mouse import move,click
+from chaojiying import Chaojiying_Client
 
 class ZhihuLoginService(BaseService):
     '''
@@ -21,7 +25,7 @@ class ZhihuLoginService(BaseService):
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         # chrome_options.add_experimental_option('useAutomationExtension', False)
         # chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        self.brower = webdriver.Chrome(executable_path="webdriver/chromedriver.exe",chrome_options=chrome_options,options=option)
+        self.brower = webdriver.Chrome(executable_path="webdriver/chromedriver.exe",chrome_options=chrome_options)
         # self.brower.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         # "source": """
         #     Object.defineProperty(navigator, 'webdriver', {
@@ -32,6 +36,7 @@ class ZhihuLoginService(BaseService):
 
     def check_login(self):
         try:
+            time.sleep(3)
             notify_element = self.brower.find_element_by_class_name("AppHeader-userInfo")
             return True
         except Exception as e:
@@ -46,11 +51,74 @@ class ZhihuLoginService(BaseService):
             self.brower.find_element_by_xpath("//input[@name='username']").send_keys("18355053764")
             self.brower.find_element_by_xpath("//input[@name='password']").send_keys(Keys.CONTROL + "a")
             self.brower.find_element_by_xpath("//input[@name='password']").send_keys("2468gggg")
-            # self.brower.find_element_by_xpath("//form//botton[@type='submit']").click()
             botton = self.brower.find_element_by_css_selector(".Button.SignFlow-submitButton.Button--primary.Button--blue")
+            try:
+                chimgcode = "null"
+                chinese_captcha_element = self.brower.find_element_by_class_name("Captcha-chineseImg")
+                base64_text = chinese_captcha_element.get_attribute("src")
+                chimgcode = base64_text.replace("data:image/jpg;base64,","").replace("%0A","")
+            except:
+                pass
+            try:
+                enimgcode = "null"
+                englsh_captcha_element = self.brower.find_element_by_class_name("Captcha-englishImg")
+                base64_text = englsh_captcha_element.get_attribute("src")
+                enimgcode = base64_text.replace("data:image/jpg;base64,","").replace("%0A","")
+            except:
+                pass
+            if chimgcode != "null":
+                with open("captcha/chincaptcha.jpeg","wb") as f:
+                    f.write(base64.b64decode(enimgcode))
+                ele_postion = chinese_captcha_element.location
+                x_relative = ele_postion["x"]
+                y_relative = ele_postion["y"]
+                brower_navigation_panel_height = self.brower.execute_script(
+                    "return window.outerHeight - window.innerHeight;"
+                )
+                z = zheye()
+                positions = z.Recognize('captcha/chincaptcha.jpeg')
+                last_position = []
+                if len(positions) == 2:
+                    last_position.append([positions[0][1],positions[0][0]])
+                    last_position.append([positions[1][1],positions[1][0]])
+                    first_position = [int(last_position[0][0] / 2),int(last_position[0][1] / 2)]
+                    second_position = [int(last_position[1][0] / 2),int(last_position[1][1] / 2)]
+                    move(x_relative + first_position[0],
+                    y_relative + brower_navigation_panel_height + first_position[1]
+                    )
+                    click()
+                    move(x_relative + second_position[0],
+                    y_relative + brower_navigation_panel_height + second_position[1]
+                    )
+                    click()
+                elif len(positions) == 1:
+                    last_position.append([positions[0][1],positions[0][0]])
+                    first_position = [int(last_position[0][0] / 2),int(last_position[0][1] / 2)]
+                    move(x_relative + first_position[0],
+                    y_relative + brower_navigation_panel_height + first_position[1]
+                    )
+                    click()
+            elif enimgcode != "null":
+                with open("captcha/engcaptcha.jpeg","wb") as f:
+                    f.write(base64.b64decode(enimgcode))
+                chaojiyingapi = Chaojiying_Client("hxltest","2468gggg","906234")
+                im = open('captcha/engcaptcha.jpeg', 'rb').read()
+                json_result = chaojiyingapi.PostPic(im,1902)
+                if json_result["err_no"] == 0:
+                    pic_str = json_result["pic_str"]
+                    print("识别码：{pic_str}".format(pic_str=pic_str))
+                else:
+                    print("识别失败")
+                    return
+                self.brower.find_element_by_xpath('//*[@id="root"]/div/main/div/div/div/div[1]/div/form/div[4]/div/div/label/input').send_keys(pic_str)
             botton.click()
-            time.sleep(5)
-            print(self.brower.page_source)
+        cookies = self.brower.get_cookies()
+        cookies_dict = {}
+        for cookie in cookies:
+            cookie_dict[cookie['name']] = cookie['value']
+        self.brower.close()
+        return cookie_dict
+
 
 if __name__ == "__main__":
     a = ZhihuLoginService()
