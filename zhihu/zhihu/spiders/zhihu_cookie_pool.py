c745fcb9e3d71071e4c280f5e25aa37a0c00628c
xuelin <19946174874@163.com>
2020-07-10 02:16:00 +0800

    fix tab

--- zhihu/zhihu/spiders/zhihu_cookie_pool.py
+++ zhihu/zhihu/spiders/zhihu_cookie_pool.py
@@ -17,12 +17,11 @@ class ZhihuCookiePoolSpider(scrapy.Spider):
         "Referer": "https://www.zhizhu.com",
         'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0"
     }
-	
-	custom_settings = {
+    custom_settings = {
         "COOKIES_ENABLED": True,
     }
-	
-	start_answer_url = ""
+
+    start_answer_url = "https://www.zhihu.com/api/v4/questions/{0}}/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset=0&platform=desktop&sort_by=default"
 
 
     def parse(self, response):
@@ -44,49 +43,50 @@ class ZhihuCookiePoolSpider(scrapy.Spider):
             else:
                 #如果不是question页面则直接进一步跟踪
                 yield scrapy.Request(url, headers=headers, callback=self.parse)
-				
+
     def parse_question(self,response):
-		match_obj = re.search("(.*zhihu.com/question/(\d+))(/|$).*", url)
-		question_id = int(match_obj.group(2))
+        match_obj = re.search("(.*zhihu.com/question/(\d+))(/|$).*", url)
+        question_id = int(match_obj.group(2))
         item_loader = ItemLoader(item=ZhihuQuestionItem(),response=response)
         item_loader.add_css("title","h1.QuestionHeader-title::text").getall()
         item_loader.add_css("content",".QuestionHeader-detail .RichText.ztext:text").getall()
         item_loader.add_value("url",response.url)
         item_loader.add_value("question_id",question_id)
-		item_loader.add_css("comments_num",".QuestionHeader-Comment button::text").get()
-		item_loader.add_css("attention_num","..NumberBoard-itemValue::text").getall()
-		item_loader.add_css("answer_num",".List-headerText span::text").get()
-		item_loader.add_css("topics",".QuestionHeader-topics #null-toggle::text").getall()
-		item_loader.add_css("click_num","..NumberBoard-itemValue::text").getall()
-		item_loader.add_value("crawl_time",datetime.datetime.now())
+        item_loader.add_css("comments_num",".QuestionHeader-Comment button::text").get()
+        item_loader.add_css("attention_num","..NumberBoard-itemValue::text").getall()
+        item_loader.add_css("answer_num",".List-headerText span::text").get()
+        item_loader.add_css("topics",".QuestionHeader-topics #null-toggle::text").getall()
+        item_loader.add_css("click_num","..NumberBoard-itemValue::text").getall()
+        item_loader.add_value("crawl_time",datetime.datetime.now())
 		
 		question_item = item_loader.load_item()
 		
-        yield scrapy.Request(start_answer_url.format(question_id, 20, 0), headers=headers, callback=self.parse_answer)
+        yield scrapy.Request(start_answer_url.format(question_id), headers=headers, callback=self.parse_answer)
         yield question_item
-		
-	def parse_answer(self,response):
-		ans_json = json.loads(response.text)
-		is_end = ans_json["paging"]["is_end"]
+        
+    def parse_answer(self,response):
+        ans_json = json.loads(response.text)
+        is_end = ans_json["paging"]["is_end"]
         next_url = ans_json["paging"]["next"]
-		
-		for answer in ans_json["data"]:
-			answer_item = ZhihuQuestionItem()
+        
+        for answer in ans_json["data"]:
+            answer_item = ZhihuQuestionItem()
             answer_item["answer_id"] = answer["id"]
             answer_item["url"] = answer["url"]
             answer_item["question_id"] = answer["question"]["id"]
             answer_item["author_id"] = answer["author"]["id"]
-            # answer_item["parise_num"] = answer["voteup_count"]
-            # answer_item["comments_num"] = answer["comment_count"]
+            answer_item["parise_num"] = answer["voteup_count"]
+            answer_item["comments_num"] = answer["comment_count"]
             answer_item["create_time"] = answer["created"]
             answer_item["update_time"] = answer["updated_time"]
             answer_item["crawl_time"] = datetime.datetime.now()
-			
-			yield answer_item
-			
-		if not is_end:
-			# yield scrapy.Request(next_url, headers=headers, callback=self.parse_answer)
-			
+            answer_item["content"] = answer["content"]
+
+            yield answer_item
+
+        if not is_end:
+        yield scrapy.Request(next_url, headers=headers, callback=self.parse_answer)
+
     def start_requests(self):
         # return [scrapy.Request('https://www.zhihu.com/#signin', headers=self.headers, callback=self.login)]
 
